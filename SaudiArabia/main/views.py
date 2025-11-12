ec1b95c6e8165453120495206dbf3b90669615eb
Rawan-Alsalem <rs2success@gmail.com>
2025-11-12 15:21:31 +0300

    removed unwanted comments

--- SaudiArabia/main/views.py
+++ SaudiArabia/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse, JsonResponse # Import JsonResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse 
 from django.shortcuts import resolve_url
 from django.shortcuts import redirect
 
@@ -7,7 +7,6 @@ from django.shortcuts import redirect
 # Create your views here.
 
 def index_view(request: HttpRequest):
-    # Read the font_size cookie state and pass it to the template context
     font_size = request.COOKIES.get('font_size', 'normal')
     return render(request, "main/index.html", {'font_size': font_size})
 
@@ -38,17 +37,12 @@ def music_view(request: HttpRequest):
 
 # cookies view for font
 def toggle_font(request):
-    # This view is now called via AJAX/Fetch, so we return a JsonResponse
-    # instead of a redirect to prevent page reload.
     current_size = request.COOKIES.get('font_size', 'normal')
 
     if current_size == 'normal':
         new_size = 'large'
     else:
         new_size = 'normal'
-    
-    # We must respond with an HttpResponse or a subclass (like JsonResponse)
-    # to set the cookie.
     response = JsonResponse({'status': 'success', 'font_size': new_size})
     response.set_cookie('font_size', new_size)
 
